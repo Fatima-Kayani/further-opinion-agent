328899596e38283b0d9099233c93ff2608829919
Fatima-Kayani <fatimakayani78@gmail.com>
2025-07-06 16:33:41 +0500

    Embedding Chroma

--- embedding.py
+++ embedding.py
@@ -3,11 +3,10 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import SentenceTransformerEmbeddings
 from langchain.vectorstores import Chroma
 
-def chroma():
-    loader = CSVLoader(file_path="disease_ontology.csv", encoding = 'utf-8')
-    documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-    chunks = text_splitter.split_documents(documents)
-    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2") 
-    vectorstore = Chroma.from_documents(chunks, embeddings, persist_directory="./chroma_db")
-    return vectorstore
+
+loader = CSVLoader(file_path="disease_ontology.csv", encoding = 'utf-8')
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+chunks = text_splitter.split_documents(documents)
+embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2") 
+vectorstore = Chroma.from_documents(chunks, embeddings, persist_directory="./chroma_db")
